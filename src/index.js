7019047bb5f205e5180caca6d56753f37f63a13e
David Royer <droyer01@gmail.com>
2019-07-02 20:18:56 -0400

    feat(plugin): :rocket: adds my comment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import VueEditor from "@/components/VueEditor.vue";
 
 const version = "__VERSION__";
 
-console.log('From davidroyer2 - PR1')
+console.log("From davidroyer2 - PR1");
+console.log("From ME inside pr/davidroyer2/1");
 
 // Declare install function executed by Vue.use()
 export function install(Vue) {
